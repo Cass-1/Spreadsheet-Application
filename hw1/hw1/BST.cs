89cf487eef1ea690e56b4985d33c7ae382e242ff
Cass Dahle <isaacbd.personal@gmail.com>
2024-01-25 11:12:47 -0800

    added an insert helper and the insert function

--- hw1/hw1/BST.cs
+++ hw1/hw1/BST.cs
@@ -3,9 +3,36 @@ namespace hw1;
 public class BST
 {
     public Node? Root { get; set; } = null;
+    
+    // insert recursive helper
+    private bool insert_helper(int num, Node? curr)
+    {
+        if (curr == null)
+        {
+            curr = new Node(num);
+            return true;
+        }
 
+        // insert on the left
+        if (curr.number < num)
+        {
+            insert_helper(num, curr.Left);
+        }
+        // insert on the right
+        else if (curr.number > num)
+        {
+            insert_helper(num, curr.Right);
+        }
+        // duplicate
+        else
+        {
+            return false;
+        }
+    }
+
+    // insert a node
     public bool Insert(int num)
     {
-        
+        return insert_helper(num, this.Root);
     }
 }
