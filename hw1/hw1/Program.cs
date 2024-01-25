5ab59cb2b8ef7d6d0b6cd14309397fd6b1a396eb
Cass Dahle <isaacbd.personal@gmail.com>
2024-01-25 10:59:00 -0800

    added code to read in the numbers and seperate them

--- hw1/hw1/Program.cs
+++ hw1/hw1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// read in the line of numbers from the console
+string numberLine = Console.ReadLine();
+
+// seperate each of the numbers on the line and put them in an array
+string[] nums = numberLine.Split(' ');
+
+// for every number add it to the BST
+foreach (var num in nums)
+{
+    // TODO: add the numbers to the BST
+    Console.WriteLine($"num: {num}");
+}
