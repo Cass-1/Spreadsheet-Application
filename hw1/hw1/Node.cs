87d1e2f48800cf23fe900cb2010189d017491927
Cass Dahle <isaacbd.personal@gmail.com>
2024-01-25 11:49:52 -0800

    Node class: made the default value for number the int32 min value

--- hw1/hw1/Node.cs
+++ hw1/hw1/Node.cs
@@ -4,9 +4,9 @@ public class Node
 {
     public Node? Left { get; set; } = null;
     public Node? Right { get; set; } = null;
-    public int Number { get; set; } = 0;
+    public int Number { get; set; }
 
-    public Node(int num)
+    public Node(int num = Int32.MinValue)
     {
         this.Number = num;
     }
