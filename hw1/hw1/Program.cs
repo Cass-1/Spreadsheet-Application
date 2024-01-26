cc48e09c2e20bd20824d0170f90e4800be0ff25f
Cass Dahle <isaacbd.personal@gmail.com>
2024-01-25 19:25:11 -0800

    Program.cs, added bst, code to get bst information and added print statements

--- hw1/hw1/Program.cs
+++ hw1/hw1/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// print statement
+
+using hw1;
+
+Console.WriteLine("Input integer numbers [0,100] seperated by spaces");
+
+// bst
+BST bst = new BST();
+
 // read in the line of numbers from the console
 string numberLine = Console.ReadLine();
 
@@ -9,6 +18,15 @@ string[] nums = numberLine.Split(' ');
 // for every number add it to the BST
 foreach (var num in nums)
 {
-    // TODO: add the numbers to the BST
-    Console.WriteLine($"num: {num}");
+    bst.Insert(int.Parse(num));
 }
+Console.Write("\nTree contents: ");
+bst.Inorder_Traversal();
+
+Console.WriteLine("\n");
+Console.WriteLine("Tree statistics: ");
+Console.WriteLine("The tree has " + bst.Count() + " nodes");
+Console.WriteLine("The tree has " + bst.Levels() + " levels\n");
+Console.WriteLine("Minimum number of levels that a tree with " + bst.Levels() + " could have = " + bst.TheoreticalMinLevels());
+
+
