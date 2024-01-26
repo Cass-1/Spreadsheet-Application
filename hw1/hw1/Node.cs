4485a9edb43a773d6fde13133f36d69c2166458e
Cass Dahle <isaacbd.personal@gmail.com>
2024-01-25 19:32:48 -0800

    Node.cs, added some comments

--- hw1/hw1/Node.cs
+++ hw1/hw1/Node.cs
@@ -2,10 +2,18 @@ namespace hw1;
 
 public class Node
 {
+    // Atributes
+    
+    // the left child node
     public Node? Left { get; set; } = null;
+    
+    // the right child node
     public Node? Right { get; set; } = null;
+    
+    // the number the node contains
     public int Number { get; set; }
 
+    // The Constructor
     public Node(int num = Int32.MinValue)
     {
         this.Number = num;
