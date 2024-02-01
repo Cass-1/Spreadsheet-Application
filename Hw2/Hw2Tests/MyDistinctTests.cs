0828940b19612db8d9cf6526491f01e2f284dd7f
Cass Dahle <isaacbd.personal@gmail.com>
2024-02-01 14:19:49 -0800

    removed redundant comment line

--- Hw2/Hw2Tests/MyDistinctTests.cs
+++ Hw2/Hw2Tests/MyDistinctTests.cs
@@ -8,8 +8,6 @@ public class MyDistinctTests
     public void Setup()
     {
     }
-
-    // MyDistinct Tests ---------------------------------------------------------------------------------------------    
     
     // usingHashSet
     [Test]
