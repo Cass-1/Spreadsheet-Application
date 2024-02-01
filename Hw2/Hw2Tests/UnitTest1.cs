2c1a5635849ea78d536dcae2d4d7d7c10e8f7fe7
Cass Dahle <isaacbd.personal@gmail.com>
2024-02-01 11:10:39 -0800

    added basic test for MyDistinct.sortFirst()

--- Hw2/Hw2Tests/UnitTest1.cs
+++ Hw2/Hw2Tests/UnitTest1.cs
@@ -48,4 +48,25 @@ public class Tests
         
         Assert.AreEqual(distinctList, myDistinctList);
     }
+    
+    [Test]
+    public void MyDistinctSortFirstBasicTest()
+    {
+        var rand = new Random();
+        var list = new List<int>();
+    
+        // make list of 10000 from 0 to 20000
+        for (int i = 0; i < 10000; i++)
+        {
+            list.Add(rand.Next(0, 20000));
+        }
+
+        // the correct answer to compare my implementation to
+        IEnumerable<int> distinctList = list.Distinct();
+        
+        // my implementation
+        var myDistinctList = MyDistinct.sortFirst(list);
+        
+        Assert.AreEqual(distinctList, myDistinctList);
+    }
 }
