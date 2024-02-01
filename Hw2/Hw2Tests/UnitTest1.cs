4132df87f62fe918d731e60f5a3a102acd4d1afb
Cass Dahle <isaacbd.personal@gmail.com>
2024-02-01 11:05:04 -0800

    added test for MyDistinct's usingHashSet method

--- Hw2/Hw2Tests/UnitTest1.cs
+++ Hw2/Hw2Tests/UnitTest1.cs
@@ -8,8 +8,23 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void MyDistinctUsingHashSetTest()
     {
-        Assert.Pass();
+        var rand = new Random();
+        var list = new List<int>();
+    
+        // make list of 10000 from 0 to 20000
+        for (int i = 0; i < 10000; i++)
+        {
+            list.Add(rand.Next(0, 20000));
+        }
+
+        // the correct answer to compare my implementation to
+        IEnumerable<int> distinctList = list.Distinct();
+        
+        // my implementation
+        var myDistinctList = MyDistinct.usingHashSet(list);
+        
+        Assert.AreEqual(distinctList, myDistinctList);
     }
 }
