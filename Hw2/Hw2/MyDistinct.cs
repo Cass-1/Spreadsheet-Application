f539ef141816b92608cd0fa6500b020cc99ffc91
Cass Dahle <isaacbd.personal@gmail.com>
2024-02-01 11:16:17 -0800

    added skeleton of MyDistinct.cs

--- Hw2/Hw2/MyDistinct.cs
+++ Hw2/Hw2/MyDistinct.cs
@@ -1,6 +1,24 @@
+using System.Collections.Generic;
+
 namespace Hw2;
 
 public class MyDistinct
 {
-    
+    public static List<int> usingHashSet(List<int> list)
+    {
+        //TODO: implement MyDistinct.usingHashSet()
+        return list;
+    }
+
+    public static List<int> o1Memory(List<int> list)
+    {
+        //TODO: implement MyDistinct.o1Memory()
+        return list;
+    }
+
+    public static List<int> sortFirst(List<int> list)
+    {
+        //TODO: implement MyDistinct.sortFirst()
+        return list;
+    }
 }
