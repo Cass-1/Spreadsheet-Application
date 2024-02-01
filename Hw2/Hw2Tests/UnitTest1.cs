ddb0749f8fd1792ee45181ea5d169a4dbed703fb
Cass Dahle <isaacbd.personal@gmail.com>
2024-02-01 11:07:53 -0800

    added basic test for MyDistinct o1Memory method

--- Hw2/Hw2Tests/UnitTest1.cs
+++ Hw2/Hw2Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ public class Tests
     }
 
     [Test]
-    public void MyDistinctUsingHashSetTest()
+    public void MyDistinctUsingHashSetBasicTest()
     {
         var rand = new Random();
         var list = new List<int>();
@@ -24,6 +24,27 @@ public class Tests
         
         // my implementation
         var myDistinctList = MyDistinct.usingHashSet(list);
+        
+        Assert.AreEqual(distinctList, myDistinctList);
+    }
+    
+    [Test]
+    public void MyDistinctO1MemoryBasicTest()
+    {
+        var rand = new Random();
+        var list = new List<int>();
+    
+        // make list of 10000 from 0 to 20000
+        for (int i = 0; i < 10000; i++)
+        {
+            list.Add(rand.Next(0, 20000));
+        }
+
+        // the correct answer to compare my implementation to
+        IEnumerable<int> distinctList = list.Distinct();
+        
+        // my implementation
+        var myDistinctList = MyDistinct.o1Memory(list);
         
         Assert.AreEqual(distinctList, myDistinctList);
     }
