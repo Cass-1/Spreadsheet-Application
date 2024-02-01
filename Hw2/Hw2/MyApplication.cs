6092ea7ac020451c060c744604dce607fba6886b
Cass Dahle <isaacbd.personal@gmail.com>
2024-02-01 10:55:22 -0800

    set up basic senario for MyApplication.cs

--- Hw2/Hw2/MyApplication.cs
+++ Hw2/Hw2/MyApplication.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Hw2;
 
@@ -6,6 +8,17 @@ public class MyApplication
 {
     public static string Run()
     {
+        var rand = new Random();
+        var list = new List<int>();
+    
+        // make list of 10000 from 0 to 20000
+        for (int i = 0; i < 10000; i++)
+        {
+            list.Add(rand.Next(0, 20000));
+        }
+        
+        
+        
         return "test";
     }
 }
