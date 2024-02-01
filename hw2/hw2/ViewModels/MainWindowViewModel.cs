66b75235506dec4c18ec2e611193a0ea8492147a
Cass Dahle <isaacbd.personal@gmail.com>
2024-02-01 09:54:56 -0800

    Updated the MainWindowViewModel so I can call the methods I create

--- hw2/hw2/ViewModels/MainWindowViewModel.cs
+++ hw2/hw2/ViewModels/MainWindowViewModel.cs
@@ -2,7 +2,14 @@
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public MainWindowViewModel()
+    {
+        Greeting = RunDistinctIntegers();
+    }
+    private string RunDistinctIntegers() // this is your method
+    {
+        //TODO: make this method
+        return "test";
+    }
+    public string Greeting { get; set;}
 }
