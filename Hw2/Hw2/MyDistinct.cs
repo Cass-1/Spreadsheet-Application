e2878a120467a3b70e3560f14a1ad74b36492894
Cass Dahle <isaacbd.personal@gmail.com>
2024-02-01 13:27:41 -0800

    added implementation for MyDistinct.o1Memory

--- Hw2/Hw2/MyDistinct.cs
+++ Hw2/Hw2/MyDistinct.cs
@@ -5,26 +5,56 @@ namespace Hw2;
 
 public class MyDistinct
 {
-    public static List<int> usingHashSet(List<int> list)
+    
+    // find the distinct elements in a list using a HashSet
+    public static int usingHashSet(List<int> list)
     {
-        //TODO: implement MyDistinct.usingHashSet()
+        //TODO: check MyDistinct.usingHashSet()
         var set = new HashSet<int>();
         foreach (var item in list)
         {
             set.Add(item);
         }
-        return set.ToList();
+        return set.ToList().Count;
     }
 
-    public static List<int> o1Memory(List<int> list)
+    // find the distinct elements in a list with storage complexity O(1)
+    public static int o1Memory(List<int> list)
     {
-        //TODO: implement MyDistinct.o1Memory()
-        return list;
+        //TODO: check MyDistinct.o1Memory()
+        
+        int distinctElements = 0;
+        bool isUnique;
+        
+        // for loop to loop through all the numbers in list in O(n^2) time
+        for (int i = 0; i < list.Count; i++)
+        {
+            isUnique = true;
+            int j = i;
+            
+            // check if any elements are the same as i in the rest of the list
+            for(; j < list.Count; j++)
+            {
+                if (i !=j && list.ElementAt(i) == list.ElementAt(j))
+                {
+                    isUnique = false;
+                    break;
+                }
+            }
+            
+            // if there are no elements the same as i in the rest of the list update the counter
+            if (isUnique)
+            {
+                distinctElements++;
+            }
+        }
+
+        return distinctElements;
     }
 
-    public static List<int> sortFirst(List<int> list)
+    public static int sortFirst(List<int> list)
     {
         //TODO: implement MyDistinct.sortFirst()
-        return list;
+        return list.Count();
     }
 }
