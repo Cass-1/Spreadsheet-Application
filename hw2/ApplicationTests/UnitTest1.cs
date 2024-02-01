66aded137a380686146647a1cac7a862f7c2f80b
Cass Dahle <isaacbd.personal@gmail.com>
2024-02-01 10:09:25 -0800

    Added NUnit test project

---
+++ hw2/ApplicationTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System.Net.Mime;
+
+namespace ApplicationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
