fe89e02abf75a1b668dab25c8f72e630472356b5
Cass-1 <cass.dahle@gmail.com>
2024-02-14 14:13:27 -0800

    Implemented DoSaveFile

--- HW3/HW3/Views/MainWindow.axaml.cs
+++ HW3/HW3/Views/MainWindow.axaml.cs
@@ -2,6 +2,7 @@
 // Copyright (c) CptS 321 Instructor. All rights reserved.
 // </copyright>
 
+using System.IO;
 using Avalonia.Platform.Storage;
 
 namespace HW3.Views;
@@ -56,17 +57,29 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
           FileTypeFilter = fileTypes,
         });
 
+        // return the file's absolute path
         interaction.SetOutput(filePath.Count == 1 ? filePath[0].Path.AbsolutePath : null);
 
     }
 
     /// <summary>
-    /// Opens a dialog to select a file in which content will be saved.
+    /// Opens a dialog to select a file or create a new file in which content will be saved.
     /// </summary>
     /// <param name="interaction">Defines the Input/Output necessary for this workflow to complete successfully.</param>
     /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
     private async Task DoSaveFile(InteractionContext<Unit, string?> interaction)
     {
-        // TODO: your code goes here.
+        // this code is from Avalonia's website https://docs.avaloniaui.net/docs/basics/user-interface/file-dialogs
+        // Get top level from the current control. Alternatively, you can use Window reference instead.
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        // Start async operation to open the dialog. This opens your sysem's default file explorer
+        var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+        {
+            Title = "Save Text File"
+        });
+        
+        // return the file's absolute path
+        interaction.SetOutput(file.Path.AbsolutePath);
     }
 }
