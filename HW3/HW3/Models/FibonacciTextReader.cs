a32b5c41abf2d55fac5864f4d538ad1a32e51a5c
Cass-1 <cass.dahle@gmail.com>
2024-02-15 20:41:28 -0800

    Added basic properties to FibonacciTextReader

--- HW3/HW3/Models/FibonacciTextReader.cs
+++ HW3/HW3/Models/FibonacciTextReader.cs
@@ -5,9 +5,27 @@ namespace HW3.Models;
 
 public class FibonacciTextReader : TextReader
 {
+    
+    // the maximum number of lines available
+    public int MaxLines { get; set; }
 
-    public FibonacciTextReader()
+    // the current position in the sequence
+    private int currentFibonacciNumber;
+
+    // constructor
+    public FibonacciTextReader(int maxLines = 0)
     {
-        
+        MaxLines = maxLines;
+        currentFibonacciNumber = 0;
+    }
+
+    public override string? ReadLine()
+    {
+        return base.ReadLine();
+    }
+
+    public override string ReadToEnd()
+    {
+        return base.ReadToEnd();
     }
 }
