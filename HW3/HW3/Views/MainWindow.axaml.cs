804918d8fc74861796d267ec131691aa50d2b768
Cass-1 <cass.dahle@gmail.com>
2024-02-15 23:05:25 -0800

    Fixed formatting

--- HW3/HW3/Views/MainWindow.axaml.cs
+++ HW3/HW3/Views/MainWindow.axaml.cs
@@ -1,19 +1,16 @@
-// <copyright file="MainWindow.axaml.cs" company="CptS 321 Instructor">
-// Copyright (c) CptS 321 Instructor. All rights reserved.
-// </copyright>
-
-using System.IO;
-using Avalonia.Platform.Storage;
+// Copyright (c) Cass Dahle. Licensed under the GPL v3.0 License. See LICENSE in the project root for license information.
 
 namespace HW3.Views;
 
 using System.Collections.Generic;
+using System.IO;
 using System.Reactive;
 using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
 using Avalonia.ReactiveUI;
-using HW3.ViewModels;
 using ReactiveUI;
+using ViewModels;
 
 /// <summary>
 /// This class handles all necessary UI events to communicate with the view model and sub windows.
@@ -25,15 +22,16 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     /// </summary>
     public MainWindow()
     {
-        InitializeComponent();
-        DataContext = new MainWindowViewModel();
+        this.InitializeComponent();
+        this.DataContext = new MainWindowViewModel();
 
         // watchers for when interactions are called
-        this.WhenActivated(d => d(ViewModel!.AskForFileToLoad.RegisterHandler(DoOpenFile)));
-        this.WhenActivated(d => d(ViewModel!.AskForFileToSave.RegisterHandler(DoSaveFile)));
+        this.WhenActivated(d => d(this.ViewModel!.AskForFileToLoad.RegisterHandler(this.DoOpenFile)));
+        this.WhenActivated(d => d(this.ViewModel!.AskForFileToSave.RegisterHandler(this.DoSaveFile)));
     }
 
     // Use the following version of DoOpenFile if you are using Avalonia 11
+
     /// <summary>
     /// Opens a dialog to select a file which will be used to load content.
     /// </summary>
@@ -50,7 +48,8 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         fileTypes.Add(fileType);
 
         // Start async operation to open the dialog.
-        var filePath = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        var filePath = await topLevel.StorageProvider.OpenFilePickerAsync(
+            new FilePickerOpenOptions
         {
           Title = "Open Text File",
           AllowMultiple = false,
@@ -59,7 +58,6 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 
         // return the file's absolute path
         interaction.SetOutput(filePath.Count == 1 ? filePath[0].Path.AbsolutePath : null);
-
     }
 
     /// <summary>
@@ -74,11 +72,12 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         var topLevel = TopLevel.GetTopLevel(this);
 
         // Start async operation to open the dialog. This opens your sysem's default file explorer
-        var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+        var file = await topLevel.StorageProvider.SaveFilePickerAsync(
+            new FilePickerSaveOptions
         {
-            Title = "Save Text File"
+            Title = "Save Text File",
         });
-        
+
         // return the file's absolute path
         interaction.SetOutput(file.Path.AbsolutePath);
     }
