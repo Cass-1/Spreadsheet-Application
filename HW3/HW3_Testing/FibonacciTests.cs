c70dd1fba2c2af823f96d89dd7e7c44b2f7781d2
Cass-1 <cass.dahle@gmail.com>
2024-02-15 20:08:45 -0800

    TDD: empty and basic test for FibonacciTextReader

---
+++ HW3/HW3_Testing/FibonacciTests.cs
@@ -0,0 +1,31 @@
+namespace HW3_Testing;
+
+using HW3.Models;
+using HW3.ViewModels;
+
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public void FibonacciTextReaderEmptyTest()
+    {
+        FibonacciTextReader fib = new FibonacciTextReader();
+        var str = fib.ReadToEnd();
+        Assert.Equals(str, null);
+    }
+
+    [Test]
+    public void FibonacciTextReaderBasicTest()
+    {
+        FibonacciTextReader fib = new FibonacciTextReader(10);
+        var str = fib.ReadToEnd();
+        var ans = "0\n1\n1\n3\n4\n5\n8\n13\n21\n34";
+        Assert.Equals(str, ans);
+    }
+}
