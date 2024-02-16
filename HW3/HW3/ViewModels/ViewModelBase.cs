9a08b6dff1a87bf7da5cb890f248b22e22553ef0
Cass-1 <cass.dahle@gmail.com>
2024-02-15 23:05:55 -0800

    Fixed formatting and bad practices

--- HW3/HW3/ViewModels/ViewModelBase.cs
+++ HW3/HW3/ViewModels/ViewModelBase.cs
@@ -1,7 +1,7 @@
-﻿using ReactiveUI;
+﻿// Copyright (c) Cass Dahle. Licensed under the GPL v3.0 License. See LICENSE in the project root for license information.
 
 namespace HW3.ViewModels;
 
-public class ViewModelBase : ReactiveObject
-{
-}
+using ReactiveUI;
+
+public class ViewModelBase : ReactiveObject;
