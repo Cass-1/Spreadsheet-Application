e86c099c8da81e26229f5215cca9c183e6e9e117
Cass-1 <cass.dahle@gmail.com>
2024-02-15 23:05:47 -0800

    Fixed formatting and bad practices

--- HW3/HW3/ViewModels/MainWindowViewModel.cs
+++ HW3/HW3/ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,20 @@
-﻿// <copyright file="MainWindow.axaml.cs" company="CptS 321 Instructor">
-// Copyright (c) CptS 321 Instructor. All rights reserved.
-// </copyright>
-
-using System.ComponentModel;
-using System.Text;
-using HW3.Models;
+﻿// Copyright (c) Cass Dahle. Licensed under the GPL v3.0 License. See LICENSE in the project root for license information.
 
 namespace HW3.ViewModels;
 
+using System.ComponentModel;
 using System.IO;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Text;
+using Models;
 using ReactiveUI;
 
+/// <summary>
+/// The main view model for the program
+/// </summary>
 public class MainWindowViewModel : ViewModelBase
 {
-    
     // private attributes
     private string fibonacciNumbers;
     private TextBody textbody;
@@ -23,50 +22,90 @@ public class MainWindowViewModel : ViewModelBase
     // a wrapper for the textbody.Text
     public string TextBodyWrapper
     {
-        get => textbody.Text;
-        set => textbody.Text = value;
+        get => this.textbody.Text;
+        set => this.textbody.Text = value;
     }
-    
+
     // interactions
     public Interaction<Unit, string?> AskForFileToLoad { get; }
+
     public Interaction<Unit, string?> AskForFileToSave { get; }
 
-    // constructor
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+    /// </summary>
     public MainWindowViewModel()
     {
-
         // Create an interaction between the view model and the view for the file to be loaded:
-        AskForFileToLoad = new Interaction<Unit, string?>();
+        this.AskForFileToLoad = new Interaction<Unit, string?>();
 
         // Similarly to load, there is a need to create an interaction for saving into a file:
-        AskForFileToSave = new Interaction<Unit, string?>();
+        this.AskForFileToSave = new Interaction<Unit, string?>();
 
         // instantiate textbody
-        textbody = new TextBody();
-        
-        // subscribe to the textbody broadcaster
-        textbody.PropertyChanged += TextBody_PropertyChanged;
+        this.textbody = new TextBody();
 
+        // subscribe to the textbody broadcaster
+        this.textbody.PropertyChanged += this.TextBody_PropertyChanged;
     }
 
     /// <summary>
-    /// Signals for the UI to update whenever the textbody class is changed
+    /// Signals for the UI to update whenever the textbody class is changed.
     /// </summary>
-    /// <param name="sender">the object sending the signal</param>
-    /// <param name="e">any arguments</param>
+    /// <param name="sender">the object sending the signal.</param>
+    /// <param name="e">any arguments.</param>
     private void TextBody_PropertyChanged(object? sender, PropertyChangedEventArgs e)
     {
-        this.RaisePropertyChanged("TextBodyWrapper");
+        this.RaisePropertyChanged(nameof(this.TextBodyWrapper));
+    }
+
+    // other code...
+
+    /// <summary>
+    /// Loads text from a file and puts it into the UI.
+    /// </summary>
+    /// <param name="textReader">the file to read from.</param>
+    private void LoadText(StreamReader textReader)
+    {
+        // TODO: make tests and implement
+        this.TextBodyWrapper = textReader.ReadToEnd();
     }
 
     /// <summary>
-    /// This is a property that will notify the user interface when changed.
-    /// TODO: You need to bind this property in the .axaml file
+    /// Reads text from the UI and saves it to a file.
     /// </summary>
-    public string FibonacciNumbers
+    /// <param name="textWriter">the file to save to.</param>
+    private void SaveText(StreamWriter textWriter)
     {
-        get => fibonacciNumbers;
-        private set => this.RaiseAndSetIfChanged(ref fibonacciNumbers, value);
+        // TODO: make tests and implement
+        textWriter.Write(this.TextBodyWrapper);
+    }
+
+    /// <summary>
+    /// Takes a string and turns it into a stream
+    /// Code retrived from https://code-maze.com/dotnet-generate-stream-from-string/.
+    /// </summary>
+    /// <param name="str">the string to convert.</param>
+    /// <param name="encoding">the encoding type.</param>
+    /// <returns>a stream version of the inputted string.</returns>
+    private StreamReader StringToStreamReader(string str, Encoding? encoding = null)
+    {
+        encoding ??= Encoding.UTF8;
+
+        var byteArray = encoding.GetBytes(str);
+        var memoryStream = new MemoryStream(byteArray);
+        var streamReader = new StreamReader(memoryStream);
+        return streamReader;
+    }
+
+    /// <summary>
+    /// Gets or sets this is a property that will notify the user interface when changed.
+    /// TODO: You need to bind this property in the .axaml file.
+    /// </summary>
+    private string FibonacciNumbers
+    {
+        get => this.fibonacciNumbers;
+        set => this.RaiseAndSetIfChanged(ref this.fibonacciNumbers, value);
     }
 
     /// <summary>
@@ -75,98 +114,65 @@ public class MainWindowViewModel : ViewModelBase
     public async void LoadFromFile()
     {
         // Wait for the user to select the file to load from.
-        var filePath = await AskForFileToLoad.Handle(default);
-        if (filePath == null) return;
+        var filePath = await this.AskForFileToLoad.Handle(default);
+        if (filePath == null)
+        {
+            return;
+        }
 
         // If the user selected a file, create the stream reader and load the text.
         var textReader = new StreamReader(filePath);
-        LoadText(textReader);
+        this.LoadText(textReader);
         textReader.Close();
     }
-    
+
     /// <summary>
     /// This method will be executed when a user wants to save content to a file.
     /// </summary>
     public async void SaveToFile()
     {
         // Wait for the user to select the file to save to.
-        var filePath = await AskForFileToSave.Handle(default);
-        if (filePath == null) return;
-        
+        var filePath = await this.AskForFileToSave.Handle(default);
+        if (filePath == null)
+        {
+            return;
+        }
+
         // If the user selected a file create new stream writer and save the text
         var textWriter = new StreamWriter(filePath);
-        SaveText(textWriter);
+        this.SaveText(textWriter);
         textWriter.Close();
-    }
-    
-    // other code...
-    /// <summary>
-    /// Loads text from a file and puts it into the UI
-    /// </summary>
-    /// <param name="textReader">the file to read from</param>
-    private void LoadText(StreamReader textReader)
-    {
-        //TODO: make tests and implement
-        TextBodyWrapper = textReader.ReadToEnd();
-    }
-    
-    /// <summary>
-    /// Reads text from the UI and saves it to a file
-    /// </summary>
-    /// <param name="textWriter">the file to save to</param>
-    private void SaveText(StreamWriter textWriter)
-    {
-        //TODO: make tests and implement
-        textWriter.Write(TextBodyWrapper);
     }
 
     /// <summary>
-    /// loads the first 50 numbers of the fibonacci sequence to the UI textbox
+    /// loads the first 50 numbers of the fibonacci sequence to the UI textbox.
     /// </summary>
     public void FibonacciFifty()
     {
         // get the fibonacci numbers in string form
         FibonacciTextReader fib = new FibonacciTextReader(50);
-        FibonacciNumbers = fib.ReadToEnd();
-        
+        this.FibonacciNumbers = fib.ReadToEnd();
+
         // convert the string to a streamreader
-        var fibonacciStreamReader = stringToStreamReader(FibonacciNumbers);
-        
+        var fibonacciStreamReader = this.StringToStreamReader(this.FibonacciNumbers);
+
         // load the text
-        LoadText(fibonacciStreamReader);
+        this.LoadText(fibonacciStreamReader);
     }
-    
+
     /// <summary>
-    /// loads the first 100 numbers of the fibonacci sequence to the UI textbox
+    /// loads the first 100 numbers of the fibonacci sequence to the UI textbox.
     /// </summary>
     public void FibonacciHundred()
     {
         // get the fibonacci numbers in string form
         FibonacciTextReader fib = new FibonacciTextReader(100);
-        FibonacciNumbers = fib.ReadToEnd();
-        
+        this.FibonacciNumbers = fib.ReadToEnd();
+
         // convert the string to a streamreader
-        var fibonacciStreamReader = stringToStreamReader(FibonacciNumbers);
-        
+        var fibonacciStreamReader = this.StringToStreamReader(this.FibonacciNumbers);
+
         // load the text
-        LoadText(fibonacciStreamReader);
+        this.LoadText(fibonacciStreamReader);
     }
-
-    /// <summary>
-    /// Takes a string and turns it into a stream
-    /// Code retrived from https://code-maze.com/dotnet-generate-stream-from-string/
-    /// </summary>
-    /// <param name="str">the string to convert</param>
-    /// <param name="encoding">the encoding type</param>
-    /// <returns>a stream version of the inputted string</returns>
-    private StreamReader stringToStreamReader(string str, Encoding? encoding = null)
-    {
-        encoding ??= Encoding.UTF8;
-        
-        var byteArray = encoding.GetBytes(str);
-        var memoryStream = new MemoryStream(byteArray);
-        var streamReader = new StreamReader(memoryStream);
-        return streamReader;
-    }
-
 }
