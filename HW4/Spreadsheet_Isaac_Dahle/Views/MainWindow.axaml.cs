3b12d24ee8ae5751054e1393166e945d977b080c
Cass-1 <cass.dahle@gmail.com>
2024-02-21 15:40:43 -0800

    Added code to initialize the columns

--- HW4/Spreadsheet_Isaac_Dahle/Views/MainWindow.axaml.cs
+++ HW4/Spreadsheet_Isaac_Dahle/Views/MainWindow.axaml.cs
@@ -1,3 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls.Templates;
+using Avalonia.Data;
+using Avalonia.Layout;
+using Avalonia.Media;
+using SpreadsheetEngine;
+
 namespace HW4.Views;
 
 using Avalonia.ReactiveUI;
@@ -15,13 +24,8 @@ using ReactiveUI;
 /// </summary>
 public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
-    /// <summary>
-    /// A function to initialize the data grid.
-    /// </summary>
-    private void InitializeDataGrid()
-    {
-        
-    }
+    private bool _isInitialized;
+
 
     /// <summary>
     /// Initializes a new instance of the <see cref="MainWindow"/> class.
@@ -36,9 +40,46 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
             {
                 if (dataContext is MainWindowViewModel)
                 {
-                    this.InitializeDataGrid();
+                    InitializeDataGrid(SpreadsheetDataGrid);
                 }
             });
     }
+
+
+    public void InitializeDataGrid(DataGrid dataGrid)
+    {
+        if (_isInitialized) return;
+
+        // initialize A to Z columns headers since these are indexed this is not a behavior supported by default
+        var columnCount = 'Z' - 'A' + 1;
+        foreach (var columnIndex in Enumerable.Range(0, columnCount))
+        {
+            // for each column we will define the header text and
+            // the binding to use
+            var columnHeader = (char) ('A' + columnIndex);
+            var columnTemplate = new DataGridTemplateColumn
+            {
+                Header = columnHeader,
+                CellTemplate = new FuncDataTemplate<IEnumerable<Cell>>((value, namescope) =>
+                    new TextBlock
+                    {
+                        [!TextBlock.TextProperty] = new Binding($"[{columnIndex}].Value"),
+                        TextAlignment = TextAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Padding = Thickness.Parse("5,0,5,0")
+                    }),
+                CellEditingTemplate = new FuncDataTemplate<IEnumerable<Cell>>((value, namescope) =>
+                    new TextBox
+                    {
+                        [!TextBox.TextProperty] = new Binding($"[{columnIndex}].Text")
+                    }),
+            };
+            dataGrid.Columns.Add(columnTemplate);
+        }
+
+
+        _isInitialized = true;
+    }
+
 
 }
