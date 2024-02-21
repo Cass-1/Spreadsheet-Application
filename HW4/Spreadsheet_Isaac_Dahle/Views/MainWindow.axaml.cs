cf2575a0193d3fd8b47e9809c4e124aee33fe2fb
Cass-1 <cass.dahle@gmail.com>
2024-02-21 14:37:54 -0800

    Added code to setup the MainWindow

---
+++ HW4/Spreadsheet_Isaac_Dahle/Views/MainWindow.axaml.cs
@@ -0,0 +1,40 @@
+namespace HW4.Views;
+
+using Avalonia.ReactiveUI;
+using HW4.ViewModels;
+using ReactiveUI;
+using System;
+using System.Reactive.Linq;
+
+/// <summary>
+/// The maid window of the UI
+/// </summary>
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
+{
+    /// <summary>
+    /// A function to initialize the data grid.
+    /// </summary>
+    private void InitializeDataGrid()
+    {
+        
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public MainWindow()
+    {
+        this.InitializeComponent();
+        this.WhenAnyValue(x => x.DataContext)
+            .Where(dataContext => dataContext != null)
+            .Subscribe(
+                dataContext =>
+            {
+                if (dataContext is MainWindowViewModel)
+                {
+                    this.InitializeDataGrid();
+                }
+            });
+    }
+
+}
