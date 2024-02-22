c6dc44c206c34bac0a65287257541857ec97cf32
Cass-1 <cass.dahle@gmail.com>
2024-02-22 14:10:01 -0800

    Removed misplaced function and fixed constructor

--- HW4/Spreadsheet_Isaac_Dahle/Views/MainWindow.axaml.cs
+++ HW4/Spreadsheet_Isaac_Dahle/Views/MainWindow.axaml.cs
@@ -13,54 +13,17 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     /// </summary>
     public MainWindow()
     {
+        // basic initalization
         this.InitializeComponent();
-        this.WhenAnyValue(x => x.DataContext)
-            .Where(dataContext => dataContext != null)
-            .Subscribe(
-                dataContext =>
-            {
-                if (dataContext is MainWindowViewModel)
-                {
-                    InitializeDataGrid(SpreadsheetDataGrid);
-                }
-            });
-    }
 
-
-    public void InitializeDataGrid(DataGrid dataGrid)
-    {
-        if (_isInitialized) return;
-
-        // initialize A to Z columns headers since these are indexed this is not a behavior supported by default
-        var columnCount = 'Z' - 'A' + 1;
-        foreach (var columnIndex in Enumerable.Range(0, columnCount))
+        // code for initalizing the SpreadsheetDataGrid
+        this.DataContextChanged += (sender, args) =>
         {
-            // for each column we will define the header text and
-            // the binding to use
-            var columnHeader = (char) ('A' + columnIndex);
-            var columnTemplate = new DataGridTemplateColumn
+            if (this.DataContext is MainWindowViewModel viewModel)
             {
-                Header = columnHeader,
-                CellTemplate = new FuncDataTemplate<IEnumerable<Cell>>((value, namescope) =>
-                    new TextBlock
-                    {
-                        [!TextBlock.TextProperty] = new Binding($"[{columnIndex}].Value"),
-                        TextAlignment = TextAlignment.Left,
-                        VerticalAlignment = VerticalAlignment.Center,
-                        Padding = Thickness.Parse("5,0,5,0")
-                    }),
-                CellEditingTemplate = new FuncDataTemplate<IEnumerable<Cell>>((value, namescope) =>
-                    new TextBox
-                    {
-                        [!TextBox.TextProperty] = new Binding($"[{columnIndex}].Text")
-                    }),
-            };
-            dataGrid.Columns.Add(columnTemplate);
-        }
-
-
-        _isInitialized = true;
+                // SpreadsheetDataGrid is defined in MainWindow.axaml
+                viewModel.InitializeDataGrid(this.SpreadsheetDataGrid);
+            }
+        };
     }
-
-
 }
