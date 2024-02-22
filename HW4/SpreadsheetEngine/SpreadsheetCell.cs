bc59d58f4789450eb839e2d279b221391dbbbb42
Cass-1 <cass.dahle@gmail.com>
2024-02-22 15:06:59 -0800

    commented out SetField

--- HW4/SpreadsheetEngine/SpreadsheetCell.cs
+++ HW4/SpreadsheetEngine/SpreadsheetCell.cs
@@ -28,11 +28,15 @@ public class SpreadsheetCell : Cell, INotifyPropertyChanged
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
-    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(propertyName);
-        return true;
-    }
+    // protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    // {
+    //     if (EqualityComparer<T>.Default.Equals(field, value))
+    //     {
+    //         return false;
+    //     }
+    //
+    //     field = value;
+    //     this.OnPropertyChanged(propertyName);
+    //     return true;
+    // }
 }
