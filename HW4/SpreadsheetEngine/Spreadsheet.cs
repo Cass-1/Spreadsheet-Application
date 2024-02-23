b16199479728af118474eff7d27c759d8218696d
Cass-1 <cass.dahle@gmail.com>
2024-02-22 16:13:13 -0800

    Added constructor, CellPropertyChanged and basic attributes

--- HW4/SpreadsheetEngine/Spreadsheet.cs
+++ HW4/SpreadsheetEngine/Spreadsheet.cs
@@ -5,6 +5,37 @@ namespace SpreadsheetEngine;
 
 public class Spreadsheet
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Spreadsheet"/> class.
+    /// The Constructor
+    /// </summary>
+    public Spreadsheet(int rowCount, int columnCount)
+    {
+        // initalize row and column count
+        this.RowCount = rowCount;
+        this.ColumnCount = columnCount;
+        
+        
+    }
     
+    /// <summary>
+    /// Gets the number of rows.
+    /// </summary>
+    public int RowCount { get; }
+    
+    /// <summary>
+    /// Gets the number of comumns.
+    /// </summary>
+    public int ColumnCount { get; }
+
+    /// <summary>
+    /// The grid of cells in the spreadsheet.
+    /// </summary>
+    public Cell[][] cellGrid;
+
+    private void CellPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
 }
