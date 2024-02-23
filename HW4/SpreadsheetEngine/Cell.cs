84be3943328b85539129523f43c3f269a7508bb1
Cass-1 <cass.dahle@gmail.com>
2024-02-22 16:15:11 -0800

    Not that SpreadsheetCell is an private class of Spreadsheet I updated Cell

--- HW4/SpreadsheetEngine/Cell.cs
+++ HW4/SpreadsheetEngine/Cell.cs
@@ -1,10 +1,35 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace SpreadsheetEngine;
 
-public abstract class Cell
+public abstract class Cell : INotifyPropertyChanged
 {
-    public int RowIndex;
-    public int ColumnIndex;
+    
+    public Cell(int rowIndex, int columnIndex)
+    {
+        RowIndex = rowIndex;
+        ColumnIndex = columnIndex;
+    }
+    // TODO: implement getters
+    public int RowIndex { get; set; }
+    public int ColumnIndex { get; set; }
 
-    protected string Text;
-    protected string Value;
+    protected string Text { get; }
+    protected string Value { get; }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
 }
