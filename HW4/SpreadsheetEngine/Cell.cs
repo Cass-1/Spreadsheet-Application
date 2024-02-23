42192fdff1932c6ba3152aea7601c140fd32bfb9
Cass-1 <cass.dahle@gmail.com>
2024-02-22 23:51:26 -0800

    Implemented Cell's Text and Value attributes

--- HW4/SpreadsheetEngine/Cell.cs
+++ HW4/SpreadsheetEngine/Cell.cs
@@ -1,18 +1,54 @@
 using System.ComponentModel;
+using System.Data;
 using System.Runtime.CompilerServices;
 
 namespace SpreadsheetEngine;
 
 public abstract class Cell : INotifyPropertyChanged
 {
+
+    private string _text;
+    protected string _value;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Cell"/> class.
+    /// </summary>
+    /// <param name="rowIndex">The cell's row index in a spreadsheet.</param>
+    /// <param name="columnIndex">The cell's column index in a spreadsheet.</param>
     public Cell(int rowIndex, int columnIndex)
     {
         this.RowIndex = rowIndex;
         this.ColumnIndex = columnIndex;
+        this._text = String.Empty;
+        this._value = String.Empty;
     }
 
     public int RowIndex { get; }
     public int ColumnIndex { get; }
+    
+    // TODO: implement text after TDD
+    protected virtual string Text
+    {
+        get => _text;
+        set
+        {
+            if (this.Text != value)
+            {
+                _text = value;
+                OnPropertyChanged(this.Text);
+            }
+        }
+    }
+
+    // TODO: implement value after TDD
+    protected virtual string Value
+    {
+        get => _value;
+        set
+        {
+            throw new ReadOnlyException(nameof(Value));
+        }
+    }
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
