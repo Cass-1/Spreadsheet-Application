0c2fda1a1d3e1a28892d7cc7af3c2e5b80c632b5
Cass-1 <cass.dahle@gmail.com>
2024-02-22 16:41:00 -0800

    removed unessisary line

--- HW4/Spreadsheet_Isaac_Dahle/ViewModels/MainWindowViewModel.cs
+++ HW4/Spreadsheet_Isaac_Dahle/ViewModels/MainWindowViewModel.cs
@@ -21,7 +21,6 @@ public class MainWindowViewModel : ViewModelBase
     private bool _isInitialized;
     private int _rowCount;
     private int _columnCount;
-    private Spreadsheet
 
     /// <summary>
     /// Gets a 2D array of Cells that is populated with the cells from the Spreadsheet.
