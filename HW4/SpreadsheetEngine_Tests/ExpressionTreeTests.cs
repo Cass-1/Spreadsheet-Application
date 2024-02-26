f5c9c35cac4a1d2ae929b5f76b147d8b9f1fa4a0
Cass-1 <cass.dahle@gmail.com>
2024-02-26 13:56:14 -0800

    TDD: added two tests for verifying variable names

--- HW4/SpreadsheetEngine_Tests/ExpressionTreeTests.cs
+++ HW4/SpreadsheetEngine_Tests/ExpressionTreeTests.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using SpreadsheetEngine;
+
 namespace SpreadsheetEngine_Tests;
 
 public class ExpressionTreeTests
@@ -34,4 +37,43 @@ public class ExpressionTreeTests
         Assert.Equals(value, 5);
     }
     
+    /// <summary>
+    /// Tests the creation of a variable with an invalid name
+    /// </summary>
+    [Test]
+    public void SetVariableInvalidBasicNameTest()
+    {
+        ExpressionTree expressionTree = new ExpressionTree();
+
+        try
+        {
+            expressionTree.SetVariable("1", 5);
+        }
+        catch (Exception e)
+        {
+            Assert.True(e is InvalidEnumArgumentException);
+        }
+
+        Assert.Fail();
+    }
+    
+    /// <summary>
+    /// Tests the creation of a variable with an invalid name
+    /// </summary>
+    [Test]
+    public void SetVariableInvalidLongNameTest()
+    {
+        ExpressionTree expressionTree = new ExpressionTree();
+
+        try
+        {
+            expressionTree.SetVariable("aaaaaa111111!asdf", 5);
+        }
+        catch (Exception e)
+        {
+            Assert.True(e is InvalidEnumArgumentException);
+        }
+
+        Assert.Fail();
+    }
 }
