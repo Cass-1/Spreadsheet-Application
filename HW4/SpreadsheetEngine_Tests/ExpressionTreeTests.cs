cead0a661b4898588959016ff162362657249690
Cass-1 <cass.dahle@gmail.com>
2024-02-26 13:54:57 -0800

    TDD: Added basic test for setting a variable

--- HW4/SpreadsheetEngine_Tests/ExpressionTreeTests.cs
+++ HW4/SpreadsheetEngine_Tests/ExpressionTreeTests.cs
@@ -2,9 +2,19 @@ namespace SpreadsheetEngine_Tests;
 
 public class ExpressionTreeTests
 {
+    /// <summary>
+    /// Tests a basic creation of a variable.
+    /// </summary>
     [Test]
     public void SetVariableBasicTest()
     {
-        Assert.Pass();
+        ExpressionTree expressionTree = new ExpressionTree();
+        
+        expressionTree.SetVariable("A1", 5);
+        
+        // TODO: be able to access private members
+        var value = expressionTree._variableDatabase["A1"];
+
+        Assert.Equals(value, 5);
     }
 }
