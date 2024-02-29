5cea43ebe3e959d638d8be81f0c2c9334f94cd65
Cass-1 <cass.dahle@gmail.com>
2024-02-28 17:54:50 -0800

    Added addition and multiplication operator nodes

---
+++ HW4/SpreadsheetEngine/AdditionOperatorNode.cs
@@ -0,0 +1,9 @@
+namespace SpreadsheetEngine;
+
+public class AdditionOperatorNode : OperatorNode
+{
+    public override double Evaluate()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ HW4/SpreadsheetEngine/MultiplicationOperatorNode.cs
@@ -0,0 +1,10 @@
+namespace SpreadsheetEngine;
+
+public class MultiplicationOperatorNode : OperatorNode
+{
+    
+    public override double Evaluate()
+    {
+        throw new NotImplementedException();
+    }
+}
