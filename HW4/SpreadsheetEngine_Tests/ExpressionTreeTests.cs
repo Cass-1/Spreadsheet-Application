de603847ac2e033d6884432e9a83143d43842bc6
Cass-1 <cass.dahle@gmail.com>
2024-02-28 17:52:55 -0800

    TDD: started some TokenizeExpression tests

--- HW4/SpreadsheetEngine_Tests/ExpressionTreeTests.cs
+++ HW4/SpreadsheetEngine_Tests/ExpressionTreeTests.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Linq.Expressions;
 using SpreadsheetEngine;
 
 namespace SpreadsheetEngine_Tests;
@@ -20,7 +21,6 @@ public class ExpressionTreeTests
 
         Assert.Equals(value, 5);
     }
-    
    
     /// <summary>
     /// Testing the creation of a variable with a long name.
@@ -32,7 +32,7 @@ public class ExpressionTreeTests
         
         expressionTree.SetVariable("a123123a342a4", 5);
         
-        var value = expressionTree._variableDatabase["a123123a342a4"];
+        var value = expressionTree.GetVariable["a123123a342a4"];
 
         Assert.Equals(value, 5);
     }
@@ -86,9 +86,9 @@ public class ExpressionTreeTests
         ExpressionTree expressionTree = new ExpressionTree();
         expressionTree.SetVariable("A2", 5);
         expressionTree.SetVariable("A2", 12);
-
+        
         var value = expressionTree.GetVariable["A2"];
-
+        
         Assert.Equals(value, 12);
     }
     
@@ -119,5 +119,84 @@ public class ExpressionTreeTests
      
              Assert.Fail();
          }
+
+    [Test]
+    public void TokenizeExpressionConstantsTest()
+    {
+        ExpressionTree expressionTree = new ExpressionTree("5+1+9");
+
+        expressionTree.TokenizeExpression();
+
+        expressionTree.
+    }
+    
+    
+    [Test]
+    public void TokenizeExpressionVariablesTest()
+    {
+        ExpressionTree expressionTree = new ExpressionTree("A22222+1-A1");
+        
+        expressionTree.SetVariable("A22222", 5);
+        expressionTree.SetVariable("A1", 1);
+
+        expressionTree.TokenizeExpression();
+        
+        
+    }
+    
+    
+    [Test]
+    public void TokenizeExpressionOneValueTest()
+    {
+        ExpressionTree expressionTree = new ExpressionTree("1");
+        
+        expressionTree.TokenizeExpression();
+        
+    }
+
+    [Test]
+    public void TokenizeExpressionInvalidExpressionTest1()
+    {
+        ExpressionTree expressionTree = new ExpressionTree("++-*/");
+
+        try
+        {
+            expressionTree.TokenizeExpression();
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }
+    
+    [Test]
+    public void TokenizeExpressionInvalidExpressionTest2()
+    {
+        ExpressionTree expressionTree = new ExpressionTree("*");
+
+        try
+        {
+            expressionTree.TokenizeExpression();
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }
+    
+    [Test]
+    public void TokenizeExpressionInvalidExpressionTest3()
+    {
+        ExpressionTree expressionTree = new ExpressionTree("1+A2+5-3*/");
+
+        try
+        {
+            expressionTree.TokenizeExpression();
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }
 }
 
