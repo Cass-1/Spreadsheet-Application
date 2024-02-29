08680756661ba93f2d2a0865f1f2f023a04d09a3
Cass-1 <cass.dahle@gmail.com>
2024-02-28 19:36:17 -0800

    Implemented AdditionOperatorNode and MultiplicationOperatorNode

--- HW4/SpreadsheetEngine/AdditionOperatorNode.cs
+++ HW4/SpreadsheetEngine/AdditionOperatorNode.cs
@@ -2,8 +2,22 @@ namespace SpreadsheetEngine;
 
 public class AdditionOperatorNode : OperatorNode
 {
+     /// <summary>
+    /// The left child node.
+    /// </summary>
+    private Node leftChild;
+
+    /// <summary>
+    /// The right child node.
+    /// </summary>
+    private Node rightChild;
+    
+    /// <summary>
+    /// Evaluates the sum of the two child nodes.
+    /// </summary>
+    /// <returns>The sum of the two child nodes.</returns>
     public override double Evaluate()
     {
-        throw new NotImplementedException();
+        return leftChild.Evaluate() + rightChild.Evaluate();
     }
 }
