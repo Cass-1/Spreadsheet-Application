ee0cb25b3fcd5d5f85ee8119497a6f983ae33316
Cass-1 <cass.dahle@gmail.com>
2024-02-29 18:22:27 -0800

    Cleaned up code and added more comments

--- HW4/SpreadsheetEngine/Cell.cs
+++ HW4/SpreadsheetEngine/Cell.cs
@@ -1,11 +1,28 @@
+// <copyright file="Cell.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
 namespace SpreadsheetEngine;
 
 using System.ComponentModel;
-using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
+/// <summary>
+/// Represents a cell in a spreadsheet.
+/// </summary>
+[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "<both the value and text property need to be protected and not private to comply with assignment requirements.>")]
 public abstract class Cell : INotifyPropertyChanged
 {
+    /// <summary>
+    /// Represents the actual text put into the cell.
+    /// </summary>
+    protected string text;
+
+    /// <summary>
+    /// Represents the evaluated result of a cell.
+    /// </summary>
+    protected string value;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Cell"/> class.
@@ -20,39 +37,61 @@ public abstract class Cell : INotifyPropertyChanged
         this.value = string.Empty;
     }
 
+    /// <inheritdoc/>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    /// Gets the cell's row index.
+    /// </summary>
     public int RowIndex { get; }
+
+    /// <summary>
+    /// Gets the cell's column index.
+    /// </summary>
     public int ColumnIndex { get; }
 
-    protected string text;
-    protected string value;
-    
+    /// <summary>
+    /// Gets or sets text.
+    /// </summary>
     public string Text
     {
-        get => text;
+        get => this.text;
         set
         {
             this.SetandNotifyIfChanged(ref this.text, value);
         }
     }
 
-    public virtual string Value { get; protected internal set; }
+    /// <summary>
+    /// Gets or sets value.
+    /// </summary>
+    public virtual string Value { get; protected internal set; } = null!;
 
-    /// <inheritdoc/>
-    public event PropertyChangedEventHandler? PropertyChanged;
-
+    /// <summary>
+    /// Calls the PropertyChangedEvent when a property is changed.
+    /// </summary>
+    /// <param name="propertyName">The name of the property that was changed.</param>
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
-    protected bool SetandNotifyIfChanged<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    /// <summary>
+    /// Sets a value and then calls OnPropertyChanged.
+    /// </summary>
+    /// <param name="field">A reference to the field that will be updated.</param>
+    /// <param name="newValue">The value to update the field to.</param>
+    /// <param name="propertyName">The name of the property.</param>
+    /// <typeparam name="T">The type of the value to update the field to.</typeparam>
+    /// <returns>If the setting of the field was successful.</returns>
+    protected bool SetandNotifyIfChanged<T>(ref T field, T newValue, [CallerMemberName] string? propertyName = null)
     {
-        if (EqualityComparer<T>.Default.Equals(field, value))
+        if (EqualityComparer<T>.Default.Equals(field, newValue))
         {
             return false;
         }
 
-        field = value;
+        field = newValue;
         this.OnPropertyChanged(propertyName);
         return true;
     }
