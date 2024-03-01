52cf6fd1454158fc3e1dca49803d927ea0f36956
Cass-1 <cass.dahle@gmail.com>
2024-03-01 09:49:36 -0800

    Fixed style and added comments for OperatorNodeFactory class

--- HW4/SpreadsheetEngine/OperatorNodeFactory.cs
+++ HW4/SpreadsheetEngine/OperatorNodeFactory.cs
@@ -1,55 +1,52 @@
-using System.Numerics;
+// Copyright (c) Cass Dahle 11775278.
+// Licensed under the GPL v3.0 License. See LICENSE in the project root for license information.
 
 namespace SpreadsheetEngine;
-using System.Reflection;
 
 /// <summary>
 /// This class is a factory for OperatorNodes.
 /// </summary>
 public class OperatorNodeFactory
 {
-    
     /// <summary>
     /// A dictionary that keeps track of the supported operations and the class types that represent them.
     /// </summary>
-    private Dictionary<char, Type> operatorTypes= new Dictionary<char, Type>();
+    private readonly Dictionary<char, Type> operatorTypes = new Dictionary<char, Type>();
 
     /// <summary>
     /// Initializes a new instance of the <see cref="OperatorNodeFactory"/> class.
     /// </summary>
-    /// <param name="str">The section of a mathmatical expression to creeate a node from </param>
     public OperatorNodeFactory()
     {
-        
         // TODO: add more types
-        operatorTypes.Add('*', typeof(MultiplicationOperatorNode));
-        operatorTypes.Add('+', typeof(AdditionOperatorNode));
-        operatorTypes.Add('-', typeof(SubtractionOperatorNode));
+        this.operatorTypes.Add('*', typeof(MultiplicationOperatorNode));
+        this.operatorTypes.Add('+', typeof(AdditionOperatorNode));
+        this.operatorTypes.Add('-', typeof(SubtractionOperatorNode));
     }
 
     /// <summary>
     /// Checks if a given operator is supported.
     /// </summary>
     /// <param name="op">The operator to check.</param>
-    /// <returns>True or false</returns>
+    /// <returns>True or false.</returns>
     public bool IsOperator(char op)
     {
-        return operatorTypes.ContainsKey(op);
+        return this.operatorTypes.ContainsKey(op);
     }
-    
+
     /// <summary>
     /// Returns an OperatorNode based on the operator token.
     /// </summary>
     /// <param name="op">The operator character.</param>
     /// <returns>An operator node.</returns>
     /// <exception cref="InvalidOperationException">Thrown when the operator given is not supported.</exception>
-    public OperatorNode? CreateOperatorNode(char op)
+    public OperatorNode CreateOperatorNode(char op)
     {
-        if(!IsOperator(op))
+        if (!this.IsOperator(op))
         {
             throw new InvalidOperationException();
         }
 
-        return (OperatorNode)System.Activator.CreateInstance(operatorTypes[op]);
+        return (OperatorNode)Activator.CreateInstance(this.operatorTypes[op])!;
     }
 }
