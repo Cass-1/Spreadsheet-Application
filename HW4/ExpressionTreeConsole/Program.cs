97e533a93d3ffc7f97aea7d08261aea2d2f6f338
Cass-1 <cass.dahle@gmail.com>
2024-03-07 19:29:57 -0800

    added program class

---
+++ HW4/ExpressionTreeConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SpreadsheetEngine;
+
+namespace ExpressionTreeConsole;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        Application application = new Application();
+
+        application.RunApp();
+        return 0;
+    }
+}
