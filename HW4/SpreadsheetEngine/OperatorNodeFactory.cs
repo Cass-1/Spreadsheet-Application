8a94a136bbe7d752559f4b0fa83cddf0505eea14
Cass-1 <cass.dahle@gmail.com>
2024-03-28 22:37:30 -0700

    style and comment changes

--- HW4/SpreadsheetEngine/OperatorNodeFactory.cs
+++ HW4/SpreadsheetEngine/OperatorNodeFactory.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Cass Dahle 11775278.
 // Licensed under the GPL v3.0 License. See LICENSE in the project root for license information.
 
-using System.Reflection;
-
 namespace SpreadsheetEngine;
 
 /// <summary>
@@ -13,57 +11,20 @@ public class OperatorNodeFactory
     /// <summary>
     /// A dictionary that keeps track of the supported operations and the class types that represent them.
     /// </summary>
-    private Dictionary<char, Type> operators = new Dictionary<char, Type>();
-
-    private delegate void OnOperator(char op, Type type);
+    private readonly Dictionary<char, Type> operators = new Dictionary<char, Type>();
 
     /// <summary>
     /// Initializes a new instance of the <see cref="OperatorNodeFactory"/> class.
     /// </summary>
     public OperatorNodeFactory()
     {
-        TraverseAvailableOperators((op, type) => operators.Add(op, type));
+        this.TraverseAvailableOperators((op, type) => this.operators.Add(op, type));
     }
 
-    private void TraverseAvailableOperators(OnOperator onOperator)
-    {
-
-        // get the type declaration of OperatorNode
-        var operatorNodeType = typeof(OperatorNode);
-        var two = typeof(MultiplicationOperatorNode);
-        // Iterate over all loaded assemblies:
-        foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-        {
-            // Get all types that inherit from our OperatorNode class using LINQ
-            var operatorTypes =
-                assembly.GetTypes().Where(type => type.IsSubclassOf(operatorNodeType));
-
-            // Iterate over those subclasses of OperatorNode
-            foreach (var type in operatorTypes)
-            {
-                // for each subclass, retrieve the Operator property
-                var operatorField = type.GetProperty("Operator");
-                if (operatorField != null)
-                {
-                    // Get the character of the Operator
-                    var value = operatorField.GetValue(type);
-
-                    // If “Operator” property is not static, you will need to create
-                    // an instance first and use the following code instead (or similar):
-                    // object value = operatorField.GetValue(Activator.CreateInstance(type,
-                    // new ConstantNode(0)));
-                    if (value is char)
-                    {
-                        var operatorSymbol = (char)value;
-
-                        // And invoke the function passed as parameter
-                        // with the operator symbol and the operator class
-                        onOperator(operatorSymbol, type);
-                    }
-                }
-            }
-        }
-    }
+    /// <summary>
+    /// A delegate used in TraverseAvailableOperators.
+    /// </summary>
+    private delegate void OnOperator(char op, Type type);
 
     /// <summary>
     /// Checks if a given operator is supported.
@@ -128,9 +89,9 @@ public class OperatorNodeFactory
         if (this.IsOperator(op))
         {
             object? field = this.operators[op].GetField("Precedence")?.GetValue(null);
-            if (field != null && field is int)
+            if (field is int i)
             {
-                return (int)field;
+                return i;
             }
         }
 
@@ -150,9 +111,9 @@ public class OperatorNodeFactory
         {
             // var test = this.operators[op].GetFields();
             object? field = this.operators[op].GetField("Precedence")?.GetValue(null);
-            if (field != null && field is int)
+            if (field is int i)
             {
-                return (int)field;
+                return i;
             }
         }
 
@@ -160,7 +121,7 @@ public class OperatorNodeFactory
     }
 
     /// <summary>
-    /// Gets the assosiativity of an operator.
+    /// Gets the associativity of an operator.
     /// </summary>
     /// <param name="op">The operator.</param>
     /// <returns>The assosiativity of the operator.</returns>
@@ -170,9 +131,9 @@ public class OperatorNodeFactory
         if (this.IsOperator(op))
         {
             object? field = this.operators[op].GetField("Assosiativity")?.GetValue(null);
-            if (field != null && field is string)
+            if (field is string s)
             {
-                return (string)field;
+                return s;
             }
         }
 
@@ -190,12 +151,53 @@ public class OperatorNodeFactory
         if (this.IsOperator(op.ToCharArray()[0]))
         {
             object? field = this.operators[op.ToCharArray()[0]].GetField("Assosiativity")?.GetValue(null);
-            if (field != null && field is string)
+            if (field is string s)
             {
-                return (string)field;
+                return s;
             }
         }
 
         throw new InvalidOperationException();
+    }
+
+    /// <summary>
+    /// Uses reflection to find all the supported opperators.
+    /// </summary>
+    /// <param name="onOperator">The class type to check for subclasses of.</param>
+    private void TraverseAvailableOperators(OnOperator onOperator)
+    {
+        // get the type declaration of OperatorNode
+        var operatorNodeType = typeof(OperatorNode);
+
+        // Iterate over all loaded assemblies:
+        foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            // Get all types that inherit from our OperatorNode class using LINQ
+            var operatorTypes =
+                assembly.GetTypes().Where(type => type.IsSubclassOf(operatorNodeType));
+
+            // Iterate over those subclasses of OperatorNode
+            foreach (var type in operatorTypes)
+            {
+                // for each subclass, retrieve the Operator property
+                var operatorField = type.GetProperty("Operator");
+                if (operatorField != null)
+                {
+                    // Get the character of the Operator
+                    var value = operatorField.GetValue(type);
+
+                    // If “Operator” property is not static, you will need to create
+                    // an instance first and use the following code instead (or similar):
+                    // object value = operatorField.GetValue(Activator.CreateInstance(type,
+                    // new ConstantNode(0)));
+                    if (value is char operatorSymbol)
+                    {
+                        // And invoke the function passed as parameter
+                        // with the operator symbol and the operator class
+                        onOperator(operatorSymbol, type);
+                    }
+                }
+            }
+        }
     }
 }
