b1f8a90f7e9f3d483ecb8fa422f2c8dd38460cd7
Cass-1 <cass.dahle@gmail.com>
2024-04-05 00:34:07 -0700

    Formatting and comments

--- HW4/Spreadsheet_Isaac_Dahle/ViewModels/RowViewModel.cs
+++ HW4/Spreadsheet_Isaac_Dahle/ViewModels/RowViewModel.cs
@@ -1,23 +1,55 @@
+// Copyright (c) Cass Dahle 11775278.
+// Licensed under the GPL v3.0 License. See LICENSE in the project root for license information.
+
 using System.Collections.Generic;
 using System.ComponentModel;
-using HW4.ViewModels;
 using ReactiveUI;
 
-namespace Spreadsheet_GettingStarted.ViewModels;
+namespace HW4.ViewModels;
 
+/// <summary>
+///     A view model for a row.
+/// </summary>
 public class RowViewModel : ViewModelBase
 {
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="RowViewModel" /> class.
+    /// </summary>
+    /// <param name="cells">A list of CellViewModesl.</param>
     public RowViewModel(List<CellViewModel> cells)
     {
-        Cells = cells;
-        foreach (var cell in Cells)
+        this.Cells = cells;
+        foreach (var cell in this.Cells)
         {
-            cell.PropertyChanged += CellOnPropertyChanged;
+            cell.PropertyChanged += this.CellOnPropertyChanged;
         }
-        SelfReference = this;
+
+        this.SelfReference = this;
     }
 
-    public CellViewModel this[int index] => Cells[index];
+    /// <summary>
+    ///     Gets or sets a list of CellViewModels.
+    /// </summary>
+    public List<CellViewModel> Cells { get; set; }
+
+    /// <summary>
+    ///     Gets this property provides a way to notify the value converter that it needs to update.
+    /// </summary>
+    public RowViewModel SelfReference { get; private set; }
+
+    /// <summary>
+    ///     An indexer for CellViewModel.
+    /// </summary>
+    /// <param name="index">The index.</param>
+    public CellViewModel this[int index] => this.Cells[index];
+
+    /// <summary>
+    ///     Event that fires the RaisePropertyChangedEvent.
+    /// </summary>
+    public void FireChangedEvent()
+    {
+        this.RaisePropertyChanged(nameof(this.SelfReference));
+    }
 
     private void CellOnPropertyChanged(object? sender, PropertyChangedEventArgs args)
     {
@@ -27,19 +59,9 @@ public class RowViewModel : ViewModelBase
             nameof(CellViewModel.CanEdit),
             nameof(CellViewModel.BackgroundColor),
         };
-        if (styleImpactingProperties.Contains(args.PropertyName))
-            FireChangedEvent();
-    }
-
-    public List<CellViewModel> Cells { get; set; }
-
-    /// <summary>
-    /// This property provides a way to notify the value converter that it needs to update
-    /// </summary>
-    public RowViewModel SelfReference { get; private set; }
-
-    public void FireChangedEvent()
-    {
-        this.RaisePropertyChanged(nameof(SelfReference));
+        if (args.PropertyName != null && styleImpactingProperties.Contains(args.PropertyName))
+        {
+            this.FireChangedEvent();
+        }
     }
 }
